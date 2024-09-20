0bbc38e76d2df9b252865fb85356dc4383862e60
mm1c <ammalaci@gmail.com>
2024-09-20 15:49:55 +0200

    test setup fixes

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,24 @@
 export default {
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        diagnostics: {
+          ignoreCodes: [1343],
+        },
+        astTransformers: {
+          before: [
+            {
+              path: "node_modules/ts-jest-mock-import-meta",
+              options: {
+                metaObjectReplacement: { url: "https://www.url.com" },
+              },
+            },
+          ],
+        },
+      },
+    ],
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  appName: import.meta.env.VITE_APP_NAME || "Rick And Morty App",
-  apiBaseUrl: import.meta.env.VITE_API_URL || "https://rickandmortyapi.com/api",
+  appName: import.meta.env?.VITE_APP_NAME || "Rick And Morty App",
+  apiBaseUrl: import.meta.env?.VITE_API_URL || "https://rickandmortyapi.com/api",
 };
