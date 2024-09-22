47c7a2edeff3f267dd4b673f873330887071fc8a
mm1c <ammalaci@gmail.com>
2024-09-22 09:14:00 +0200

    comments removed

--- src/themes/light.ts
+++ src/themes/light.ts
@@ -2,11 +2,7 @@ import { createTheme } from "@mui/material";
 
 const theme = createTheme({
   typography: {
-    // fontFamily: "Roboto Mono, monospace",
     fontSize: 14,
-    // fontWeightLight: 300,
-    // fontWeightRegular: 400,
-    // fontWeightMedium: 500,
   },
   palette: {
     background: {
